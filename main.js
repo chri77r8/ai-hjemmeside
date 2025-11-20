b991d4e8948a8f95578e8f15c0a9deb61d5d20a3
chris <christofferbzh@gmail.com>
2025-11-20 09:57:08 +0100

    fixed ai prompt

---
+++ main.js
@@ -0,0 +1,36 @@
+const yourApiKey = document.querySelector(".yourApiKey")
+const ingredients = document.querySelector(".ingredients")
+const foodMood = document.querySelector(".foodMood")
+const result = document.querySelector(".submit")
+
+function mistralChat(prompt, apiKey) {
+    return fetch("https://api.mistral.ai/v1/chat/completions", {
+        method: "POST",
+        headers: {
+            "Authorization": `Bearer ${apiKey}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            model: "codestral-latest",
+            messages: [
+                { role: "system", content: "You are an expert in easy and fast recipes, that must always be gluten free. You must use some of the ingredients that I give, but you are allowed to add more ingredients to the recipe. The measurements in the recipe needs to be in the metric system"},
+                { role: "user", content: prompt }
+            ],
+            temperature: 0.7,
+            max_tokens: 512
+        })
+    })
+        .then(response => response.json())
+        .then(function(data) {
+            console.log(data)
+            console.log(data.choices[0].message.content);
+        });
+}
+
+result.addEventListener("click", function ()  {
+    let apiKeyValue = yourApiKey.value
+    let foodMoodValue = foodMood.value
+    let ingredientsValue = ingredients.value
+
+    mistralChat(`${foodMoodValue}. I have: ${ingredientsValue}`, apiKeyValue)
+})
