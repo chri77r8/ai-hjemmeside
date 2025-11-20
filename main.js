1e27390e0b44e8f81e964a5eecf339091d431016
chris <christofferbzh@gmail.com>
2025-11-20 10:44:36 +0100

    current progress after class

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const yourApiKey = document.querySelector(".yourApiKey")
 const ingredients = document.querySelector(".ingredients")
 const foodMood = document.querySelector(".foodMood")
 const result = document.querySelector(".submit")
+const recipeOutput = document.querySelector("#recipe-output")
 
 function mistralChat(prompt, apiKey) {
     return fetch("https://api.mistral.ai/v1/chat/completions", {
@@ -13,24 +14,31 @@ function mistralChat(prompt, apiKey) {
         body: JSON.stringify({
             model: "codestral-latest",
             messages: [
-                { role: "system", content: "You are an expert in easy and fast recipes, that must always be gluten free. You must use some of the ingredients that I give, but you are allowed to add more ingredients to the recipe. The measurements in the recipe needs to be in the metric system"},
-                { role: "user", content: prompt }
+                {
+                    role: "system",
+                    content: "You are an expert in easy and fast recipes, that must always be gluten free. You must use some of the ingredients that I give, but you are allowed to add more ingredients to the recipe. The measurements in the recipe needs to be in the metric system"
+                },
+                {role: "user", content: prompt}
             ],
             temperature: 0.7,
             max_tokens: 512
         })
     })
         .then(response => response.json())
-        .then(function(data) {
+        .then(function (data) {
+            let dataChoices = data.choices[0].message.content;
             console.log(data)
-            console.log(data.choices[0].message.content);
+            console.log(dataChoices);
+            recipeOutput.innerText = dataChoices
+
         });
 }
 
-result.addEventListener("click", function ()  {
+result.addEventListener("click", function () {
     let apiKeyValue = yourApiKey.value
     let foodMoodValue = foodMood.value
     let ingredientsValue = ingredients.value
 
     mistralChat(`${foodMoodValue}. I have: ${ingredientsValue}`, apiKeyValue)
 })
+
